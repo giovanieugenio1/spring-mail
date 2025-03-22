fefb8d092a09856e39d45d792ac7baff19e427e9
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-03-22 12:37:11 -0300

    feat: criado o metodo sendEmail no controller

--- src/main/java/com/service_mail/spring_mail/controller/EmailController.java
+++ src/main/java/com/service_mail/spring_mail/controller/EmailController.java
@@ -1,11 +1,26 @@
 package com.service_mail.spring_mail.controller;
 
+import com.service_mail.spring_mail.service.IEmailService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class EmailController {
+
+    private final IEmailService emailService;
+
+    private static final Logger log = LoggerFactory.getLogger(EmailController.class);
+
+    public EmailController(IEmailService emailService) {
+        this.emailService = emailService;
+    }
 
     @PostMapping
     public String sendEmail(String to, String[] cc, String subjet, String body) {
-        return "mail send";
+        String result = emailService.sendEmail(to, cc, subjet, body);
+        log.info("Email sent successfully :: ", result);
+        return result;
     }
 }
