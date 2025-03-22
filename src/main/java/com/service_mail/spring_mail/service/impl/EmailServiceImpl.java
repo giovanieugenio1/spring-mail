e7dec29ced400124c29776943be7c3b192f1cd37
Giovani e Jesuino <giovani.eugenio.sp@gmail.com>
2025-03-22 12:32:00 -0300

    feat: criado o metodo sendEmail no service

--- src/main/java/com/service_mail/spring_mail/service/impl/EmailServiceImpl.java
+++ src/main/java/com/service_mail/spring_mail/service/impl/EmailServiceImpl.java
@@ -1,6 +1,7 @@
 package com.service_mail.spring_mail.service.impl;
 
 import com.service_mail.spring_mail.service.IEmailService;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Service;
 
@@ -9,12 +10,21 @@ public class EmailServiceImpl implements IEmailService {
 
     private final JavaMailSenderImpl javaMailSender;
 
+
     public EmailServiceImpl(JavaMailSenderImpl javaMailSender) {
         this.javaMailSender = javaMailSender;
     }
 
     @Override
     public String sendEmail(String to, String[] cc, String subjet, String body) {
-        return "";
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setCc(cc);
+        message.setSubject(subjet);
+        message.setText(body);
+
+        javaMailSender.send(message);
+
+        return "Mail sent successfully !";
     }
 }
